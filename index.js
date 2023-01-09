ed6d5d39ec92cd2721a4d21a60bab46800744935
timabloom <timabloom@proton.me>
2023-01-09 15:18:38 +0100

    setup nodemailer with Ethereal a fake SMTP service

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const express = require("express");
-const app = express();
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
 
+const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post("/", async (req, res) => {
   res.send("Server responding");
+  const { email } = req.body;
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false,
+    auth: {
+      user: "marisol58@ethereal.email",
+      pass: "CBMKvaSjZ8mzKqA521",
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: `${email}`,
+    subject: "Hello ✔",
+    text: "Hello world?",
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 });
 
 app.listen(PORT, () => {
