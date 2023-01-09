1273b66feeef1abb7b1f4a2bdefc2038e46c55bb
timabloom <timabloom@proton.me>
2023-01-09 15:22:35 +0100

    removed comments

--- index.js
+++ index.js
@@ -23,14 +23,13 @@ app.post("/", async (req, res) => {
   });
 
   let info = await transporter.sendMail({
-    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    from: '"Fred Foo 👻" <foo@example.com>',
     to: `${email}`,
     subject: "Hello ✔",
     text: "Hello world?",
   });
 
   console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 });
 
 app.listen(PORT, () => {
